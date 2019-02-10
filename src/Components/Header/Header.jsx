300f3a9bb24b85cca245b9ff6515815708249202
Charlie <phunghiep2709@gmail.com>
2019-02-10 21:45:02 +0700

    app

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Header.scss";
+
+export default function Header() {
+  return (
+    <header className="header">
+    <img src="/img/header-img.jpg" className="header-background"/>
+      <div className="header-content">
+        <h1 className="header-heading-1">WE ARE TELIA</h1>
+        <h5 className="header-heading-5">
+          Even better services both in and outside Finland.
+        </h5>
+      </div>
+      <picture className="header-icon">
+        <img src="/img/header-icon.png" />
+      </picture>
+    </header>
+  );
+}
