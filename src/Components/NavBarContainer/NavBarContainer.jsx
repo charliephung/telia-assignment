3fdde5b70b9572e88078259baaac7f0efd6cf1e3
Charlie <phunghiep2709@gmail.com>
2019-02-14 05:49:24 +0700

    update navcontainer

--- src/Components/NavBarContainer/NavBarContainer.jsx
+++ src/Components/NavBarContainer/NavBarContainer.jsx
@@ -1,35 +1,77 @@
 import React, { Component } from "react";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import classnames from "classnames";
+import Container from "Commons/Container/Container";
 import Section from "Features/Section/Section";
-import Menu from "./Menu/Menu";
+import LoginForm from "./LoginForm/LoginForm";
 import NavBar from "./NavBar/NavBar";
+import Menu from "./Menu/Menu";
+
 import "./NavBarContainer.scss";
 
 export default class NavBarContainer extends Component {
-  state = { openMenu: true };
+  state = { activeItem: 1 };
+
+  onItemClick = activeItem => {
+    this.setState(state =>
+      activeItem === state.activeItem ? { activeItem: null } : { activeItem }
+    );
+  };
+
   render() {
-    const { openMenu } = this.state;
+    const { activeItem } = this.state;
+    const NavBarDropDown = [Menu, LoginForm][activeItem];
     return (
       <>
         <Section config={{ threshold: 1 }}>
-          {({ entry }) =>
-            entry.intersectionRatio === 1 ? (
-              <div className="navbar-container--fixed">
-                <NavBar
-                  onChange={openMenu => this.setState(() => ({ openMenu }))}
-                  showLanguage={true}
-                />
-              </div>
-            ) : (
-              <div className="navbar-container--fixed">
-                <NavBar
-                  onChange={openMenu => this.setState(() => ({ openMenu }))}
-                />
-              </div>
-            )
-          }
+          {({ entry }) => (
+            <Container
+              addClass={classnames("navbar__container--fixed", "box-shadow-1")}
+            >
+              <NavBar
+                activeItem={activeItem}
+                onItemClick={this.onItemClick}
+                showLanguage={entry.intersectionRatio === 1 ? true : false}
+              />
+            </Container>
+          )}
         </Section>
-        {openMenu && <Menu />}
+        <TransitionGroup>
+          {NavBarDropDown && [
+            <CSSTransition timeout={300} key={activeItem} classNames="drop">
+              <NavBarDropDown
+                style={{
+                  transition: "300ms all ease"
+                }}
+              />
+            </CSSTransition>,
+            <CSSTransition timeout={1000} key={10} classNames="fade">
+              <NavBarDropDownBackGround
+                onClick={() => this.onItemClick(null)}
+              />
+            </CSSTransition>
+          ]}
+        </TransitionGroup>
       </>
     );
   }
+}
+
+function NavBarDropDownBackGround({ addClass = "", onClick, ...rest }) {
+  return (
+    <div
+      {...rest}
+      onClick={onClick}
+      style={{
+        transition: "1000ms all ease",
+        position: "fixed",
+        top: "0",
+        left: "0",
+        zIndex: 5,
+        width: "100%",
+        height: "100%",
+        backgroundColor: "rgba(0, 0, 0, 0.3)"
+      }}
+    />
+  );
 }
