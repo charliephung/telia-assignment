2a20e4f73e2cfe88101cf99c623754ce119cfa45
Charlie <phunghiep2709@gmail.com>
2019-02-14 18:36:43 +0700

    update login form

--- src/Components/NavBarContainer/LoginForm/LoginForm.jsx
+++ src/Components/NavBarContainer/LoginForm/LoginForm.jsx
@@ -1,8 +1,13 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
+import { Email, People, FingerPrint } from "Commons/Icons";
 import Container from "Commons/Container/Container";
+import BankCodeForm from "./BankCodeForm/BankCodeForm";
+import UserNameForm from "./UserNameForm/UserNameForm";
 import MobileForm from "./MobileForm/MobileForm";
-import Nav from "Commons/Nav/Nav";
+import Slider from "Features/Slider/Slider";
 import classnames from "classnames";
+import Nav from "Commons/Nav/Nav";
+
 import "./LoginForm.scss";
 
 const navItems = [
@@ -18,11 +23,16 @@ const things = [
   "Stay connected to our customer service around the clock",
   "And much more!"
 ];
+const footerItems = [
+  [Email, "E-mail"],
+  [People, "Community"],
+  [FingerPrint, "Mobile certificate"]
+];
 
-export default class LoginForm extends Component {
+export default class LoginForm extends PureComponent {
   state = { activeItem: 0 };
 
-  onItemClick = activeItem => {
+  onLoginTabClick = activeItem => {
     this.setState(state => {
       if (state.activeItem === activeItem) return null;
       return { activeItem };
@@ -32,26 +42,25 @@ export default class LoginForm extends Component {
   render() {
     const { className = "login-form", addClass = "", ...rest } = this.props;
     const { activeItem } = this.state;
+
     const newClassName = classnames(className, addClass);
+    const Forms = [MobileForm, UserNameForm, BankCodeForm];
     return (
       <div {...rest} className={newClassName}>
         <Container
           style={{
-            backgroundColor: "white"
+            backgroundColor: "white",
+            paddingTop: "3rem"
           }}
         >
-          <br />
-          <br />
           <h1 className="color-gray-3">LOGIN</h1>
           <Nav addClass="color-gray-1">
             <Nav.List>
               {navItems.map((content, index) => (
                 <Nav.Item
-                  onClick={() => this.onItemClick(index)}
+                  onClick={() => this.onLoginTabClick(index)}
                   key={index}
-                  addClass={classnames("login-form__item", {
-                    "login-form__item": index === activeItem
-                  })}
+                  addClass={"login-form__item"}
                 >
                   {content}
                 </Nav.Item>
@@ -67,16 +76,44 @@ export default class LoginForm extends Component {
         </Container>
         <div className="login-form__body">
           <Container addClass="login-form__container">
-            <MobileForm />
+            <div className="login-form__container__left">
+              <Slider
+                sliderStyle={{
+                  width: "70rem"
+                }}
+                showSlide={activeItem}
+              >
+                {Forms.map((Form, index) => (
+                  <Form key={index} />
+                ))}
+              </Slider>
+            </div>
             <div />
-            <div>
+            <div className="login-form__container__right">
               <h3>Sign in and keep things right for you:</h3>
+              <br />
               <ul>
                 {things.map((content, index) => (
                   <li key={index}>{content}</li>
                 ))}
               </ul>
             </div>
+          </Container>
+          <Container
+            style={{
+              backgroundColor: "white"
+            }}
+          >
+            <Nav>
+              <Nav.List addClass="login-form__footer">
+                {footerItems.map(([Item, content], index) => (
+                  <Nav.Item addClass="login-form__footer__item" key={index}>
+                    <Item />
+                    {content}
+                  </Nav.Item>
+                ))}
+              </Nav.List>
+            </Nav>
           </Container>
         </div>
       </div>
