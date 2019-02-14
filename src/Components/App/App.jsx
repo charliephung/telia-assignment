19c4d82d8dd8d4fcfa0fa2a19e0d569d79275a00
Charlie <phunghiep2709@gmail.com>
2019-02-15 01:02:59 +0700

    add language provider

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,16 +1,17 @@
 import React, { Component } from "react";
 import Header from "Components/Header/Header";
+import { Language } from "Features/Language/Language";
 import NavBarContainer from "Components/NavBarContainer/NavBarContainer";
 import "./App.scss";
 
 class App extends Component {
   render() {
     return (
-      <>
+      <Language>
         <NavBarContainer />
         <Header />
         <div style={{ height: "2000px" }} />
-      </>
+      </Language>
     );
   }
 }
