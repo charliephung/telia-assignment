de5989bdac37622ed1daedb100604b1d42d2675c
Charlie <phunghiep2709@gmail.com>
2019-02-15 01:02:20 +0700

    add language

--- src/Commons/SearchBox/SearchBox.jsx
+++ src/Commons/SearchBox/SearchBox.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import Search from "Commons/Icons/Search";
 import "./SearchBox.scss";
 
-export default function SearchBox() {
+export default function SearchBox({ placeHolder }) {
   return (
     <div className="searchbox">
       <div className="searchbox-contain">
-        <input placeholder="Search" className="searchbox-input" />
+        <input placeholder={placeHolder} className="searchbox-input" />
         <span className="searchbox-button">
           <Search className="searchbox-icon" />
         </span>
