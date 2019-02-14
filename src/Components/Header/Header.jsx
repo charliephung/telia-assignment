1f98a78a7c18a35996bfa1a57e0badbde8623cf7
Charlie <phunghiep2709@gmail.com>
2019-02-14 21:54:01 +0700

    refactor header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 import "./Header.scss";
 
-export default function Header() {
+const Header = React.memo(function(props) {
+  const headings = [
+    <h1
+      style={{
+        transition: "1000ms all ease"
+      }}
+      className="header-heading-1 color-purple-1 "
+    >
+      WE ARE TELIA
+    </h1>,
+    <h5
+      style={{
+        transition: "500ms all ease"
+      }}
+      className="header-heading-5 color-gray-1"
+    >
+      Even better services both in and outside Finland.
+    </h5>
+  ];
   return (
     <header className="header">
       <img
@@ -10,14 +30,26 @@ export default function Header() {
         className="header-background"
       />
       <div className="header-content">
-        <h1 className="header-heading-1">WE ARE TELIA</h1>
-        <h5 className="header-heading-5">
-          Even better services both in and outside Finland.
-        </h5>
+        <TransitionGroup>
+          {headings.map((heading, index) => (
+            <CSSTransition
+              appear={true}
+              key={index}
+              timeout={500}
+              classNames="fade"
+            >
+              {heading}
+            </CSSTransition>
+          ))}
+        </TransitionGroup>
       </div>
       <picture className="header-icon">
         <img alt="headericon" src="/img/header-icon.png" />
       </picture>
     </header>
   );
-}
+});
+
+Header.propTypes = {};
+Header.defaultProps = {};
+export default Header;
