6828c27c0112419ae4247f6c7869694ce42db4c0
Charlie <phunghiep2709@gmail.com>
2019-02-14 18:36:04 +0700

    add Link to nav

--- src/Commons/Nav/Nav.jsx
+++ src/Commons/Nav/Nav.jsx
@@ -20,10 +20,21 @@ const Item = ({ className, addClass = "", children, ...rest }) => {
     </li>
   );
 };
+const Link = ({ className, addClass = "", href = "", children, ...rest }) => {
+  if (className === undefined) className = "nav-link " + addClass;
+  else className += " " + addClass;
+
+  return (
+    <a href={href} {...rest} className={className}>
+      {children}
+    </a>
+  );
+};
 
 export default class Nav extends Component {
   static List = List;
   static Item = Item;
+  static Link = Link;
 
   render() {
     let { className, addClass = "", children, ...rest } = this.props;
