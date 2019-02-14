8362e9dfadd1c2d8f1e43518be9814518cfb929d
Charlie <phunghiep2709@gmail.com>
2019-02-14 22:22:27 +0200

    Updates

---
+++ precache-manifest.5b843824bf4b66084a7a3af94fca762e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "76cdb5b29155f0140bf8",
+    "url": "/telia-assigment/static/js/0.76cdb5b2.chunk.js"
+  },
+  {
+    "revision": "27ff33078191bd5ae2f7",
+    "url": "/telia-assigment/static/css/main.d1128d17.chunk.css"
+  },
+  {
+    "revision": "27ff33078191bd5ae2f7",
+    "url": "/telia-assigment/static/js/main.27ff3307.chunk.js"
+  },
+  {
+    "revision": "c2fad4b254cf45947877",
+    "url": "/telia-assigment/static/css/2.b32063f6.chunk.css"
+  },
+  {
+    "revision": "c2fad4b254cf45947877",
+    "url": "/telia-assigment/static/js/2.c2fad4b2.chunk.js"
+  },
+  {
+    "revision": "3cbe2105e5ca64809f34",
+    "url": "/telia-assigment/static/css/3.d3240cd0.chunk.css"
+  },
+  {
+    "revision": "3cbe2105e5ca64809f34",
+    "url": "/telia-assigment/static/js/3.3cbe2105.chunk.js"
+  },
+  {
+    "revision": "dbff3be48ed66cea1500",
+    "url": "/telia-assigment/static/js/4.dbff3be4.chunk.js"
+  },
+  {
+    "revision": "cde58fcee08b527145c6",
+    "url": "/telia-assigment/static/js/runtime~main.cde58fce.js"
+  },
+  {
+    "revision": "3eb4e6d2b594c70660c051e16cf79b54",
+    "url": "/telia-assigment/index.html"
+  }
+];
