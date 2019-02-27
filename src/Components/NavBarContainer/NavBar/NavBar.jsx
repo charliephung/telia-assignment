d4b8eeb2d93011ab30274880afa9ebbbbc944659
Charlie <phunghiep2709@gmail.com>
2019-02-28 01:16:43 +0200

    remove common

--- src/Components/NavBarContainer/NavBar/NavBar.jsx
+++ src/Components/NavBarContainer/NavBar/NavBar.jsx
@@ -3,6 +3,8 @@ import PropTypes from "prop-types";
 import Nav from "Commons/Nav/Nav";
 import classnames from "classnames";
 import { Logo, User, Burger, Cart } from "Commons/Icons";
+import Container from "Commons/Container/Container";
+
 import "./NavBar.scss";
 
 const languages = ["FI", "SV", "EN"];
@@ -16,47 +18,51 @@ const NavBar = React.memo(props => {
     language,
     ...rest
   } = props;
+
   const display = language.navbar;
   const navItems = [[Burger, display.item0], [User, display.item1], [Cart, ""]];
+
   return (
-    <Nav {...rest}>
-      <Nav.List>
-        <Logo className="navbar__brand" />
-      </Nav.List>
-      <span
-        className={classnames("navbar__span", {
-          "navbar__span navbar__span--show": showLanguage
-        })}
-      >
-        <ul>
-          {languages.map((language, index) => (
-            <li
-              onClick={() => updateLanguage(language.toLowerCase())}
+    <Container addClass="bg-color-white-1 box-shadow-1">
+      <Nav {...rest}>
+        <Nav.List>
+          <Logo className="navbar__brand" />
+        </Nav.List>
+        <span
+          className={classnames("navbar__span", {
+            "navbar__span navbar__span--show": showLanguage
+          })}
+        >
+          <ul>
+            {languages.map((language, index) => (
+              <li
+                onClick={() => updateLanguage(language.toLowerCase())}
+                key={index}
+              >
+                {language}
+              </li>
+            ))}
+          </ul>
+        </span>
+        <Nav.List addClass="right">
+          {navItems.map(([Icon, content], index) => (
+            <Nav.Item
               key={index}
+              addClass={classnames("navbar__item", {
+                "navbar__item navbar__item--active": activeItem === index
+              })}
+              onClick={() => onItemClick(index)}
             >
-              {language}
-            </li>
+              <Icon
+                open={activeItem === 0 ? true : false}
+                addClass="navbar__icon"
+              />
+              <span className="navbar__item__text">{content}</span>
+            </Nav.Item>
           ))}
-        </ul>
-      </span>
-      <Nav.List addClass="right">
-        {navItems.map(([Item, content], index) => (
-          <Nav.Item
-            key={index}
-            addClass={classnames("navbar__item", {
-              "navbar__item navbar__item--active": activeItem === index
-            })}
-            onClick={() => onItemClick(index)}
-          >
-            <Item
-              open={activeItem === 0 ? true : false}
-              addClass="navbar__icon"
-            />
-            <span className="navbar__item__text">{content}</span>
-          </Nav.Item>
-        ))}
-      </Nav.List>
-    </Nav>
+        </Nav.List>
+      </Nav>
+    </Container>
   );
 });
 
